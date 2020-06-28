767117e07a8de4310c1f7e6a138866a016d479ac
RupeshMathur <mathurupesh92@gmail.com>
2020-06-29 00:08:39 +0530

    Rollback changes

--- ParkingLot_Coditas/src/com/coditas/constants/ParkingLotConstants.java
+++ ParkingLot_Coditas/src/com/coditas/constants/ParkingLotConstants.java
@@ -6,7 +6,7 @@ public interface ParkingLotConstants {
 	public String NOT_AVAILABE = "Not Available";
 	static String CREATE = "create_parking_lot";
 	static String PARK = "park";
-	static String UNPARK = "leave";
+	static String LEAVE = "leave";
 	static String STATUS = "status";
 	static double BASE_FARE = 10.0;
 	static double MIN_HOURS_FOR_BASE_FARE = 2.0;
